4899c58e312fe62b45059d7268ec5a4eb5886211
AlexGrim <alejandrogrim12@gmail.com>
2023-10-07 21:21:19 -0600

    chatbot - home design

--- lib/screens/chatbot_screen.dart
+++ lib/screens/chatbot_screen.dart
@@ -16,11 +16,14 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
   String content = 'Hi, I am a chatbot, ask me about the ocean.';
   List<Message> messages = [];
   late String apiKey;
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
     apiKey = 'AIzaSyDWnszd8THhYE-m9OH2AIripZBxboCkVao';
+    messages.add(const Message(
+        author: 'Willy 🤠', content: 'I\'m Willy, your favorite chatbot!'));
   }
 
   Future<void> chat(String message) async {
@@ -34,6 +37,7 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
     );
     final messageRequest = GenerateMessageRequest(
       prompt: messagePrompt,
+      temperature: 0.1,
     );
 
     final GeneratedMessage messageResponse =
@@ -46,9 +50,9 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
     print(messageResponse);
 
     setState(() {
-      messages.add(Message(author: 'User', content: message));
+      messages.add(Message(author: 'You 🫵', content: message));
       messages.add(Message(
-          author: 'Willy', content: messageResponse.candidates[0].content));
+          author: 'Willy 🤠', content: messageResponse.candidates[0].content));
     });
   }
 
@@ -59,69 +63,92 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
         title: const Text('Willy Bot'),
         backgroundColor: Colors.grey[900],
       ),
-      body: Container(
-        color: Colors.grey[900],
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 16, left: 8, right: 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Print content
-              Expanded(
-                child: ListView.builder(
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final message = messages[index];
-                    return ChatCard(
-                      messageText: message.content,
-                      senderName: message.author!, // set the sender name
-                      messageTimestamp: 'Number of message: $index',
-                    );
-                  },
-                ),
-              ),
-              Row(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.grey[900],
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16, left: 8, right: 8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
+                  // Print content
                   Expanded(
-                    flex:
-                        4, // Adjust the flex value to control the width of the text field.
-                    child: TextField(
-                      style: const TextStyle(color: Colors.white),
-                      controller: _controller,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 2,
-                          ),
-                        ),
-                        labelText: 'Enter a question',
-                        labelStyle: TextStyle(color: Colors.white),
-                      ),
+                    child: ListView.builder(
+                      itemCount: messages.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final message = messages[index];
+                        return ChatCard(
+                          messageText: message.content,
+                          senderName: message.author!, // set the sender name
+                          messageTimestamp: 'Number of message: $index',
+                        );
+                      },
                     ),
                   ),
-                  const SizedBox(width: 8),
-                  SizedBox(
-                    height: 48,
-                    child: Expanded(
-                      flex: 1,
-                      child: ElevatedButton(
-                          onPressed: () {
-                            chat(_controller.text);
-                            _controller.clear();
-                          },
-                          // style: ElevatedButton.styleFrom(
-                          //   backgroundColor: Colors.orange,
-                          // ),
-                          child: const Icon(Icons.send)),
-                    ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex:
+                            4, // Adjust the flex value to control the width of the text field.
+                        child: TextField(
+                          style: const TextStyle(color: Colors.white),
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                            ),
+                            labelText: 'Enter a question',
+                            labelStyle: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SizedBox(
+                        height: 48,
+                        child: Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                // Show the loading GIF
+                                setState(() {
+                                  isLoading = true;
+                                });
+
+                                // Call the chat() function
+                                await chat(_controller.text);
+                                _controller.clear();
+
+                                // Hide the loading GIF
+                                setState(() {
+                                  isLoading = false;
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                              ),
+                              child: const Icon(Icons.send)),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+
+          // Show the loading GIF if isLoading is true
+          Visibility(
+            visible: isLoading,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
     );
   }
